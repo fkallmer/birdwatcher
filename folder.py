93a5a5423599961ad634c96485c9d0b8c18ff574
fkallmer <94541193+fkallmer@users.noreply.github.com>
2022-10-03 19:55:37 +0200

    Update folder.py

--- folder.py
+++ folder.py
@@ -1,6 +1,6 @@
-import os
-import logging
-import shutil
+import os #Import OS-Bibliothek (Funktionen des Betriebssystems)
+import logging #Import Logging-Bibliothek
+import shutil #Import Shutil-Bibliothek (Datei- und Ordneroptionen)
 
 class folder:
     
@@ -11,32 +11,32 @@ class folder:
         #month = datetime.now().strftime("%m-%Y")
         
             
-    def createFolder(pPath):
+    def createFolder(pPath): # Erstellung eines Ordners, mit Pfad als Parameter
 
-        if not os.path.exists(pPath):
+        if not os.path.exists(pPath): #"Wenn Ordner noch nicht existiert"
             try:
-                os.mkdir(pPath)
+                os.mkdir(pPath) #Erstelle Ordnern
             except OSError as error:
-                logging.error(error)
+                logging.error(error) #Logging potentieller Fehlermeldung
 
-    def delFolder(pTransfer):
+    def delFolder(pTransfer): # Löschen eines Ordners, mit Pfad als Parameter
         try:
-            shutil.rmtree(pTransfer)
+            shutil.rmtree(pTransfer) # Lösche Ordner
         except Exception as e:
-            logging.error(e)
+            logging.error(e) #Logging potentieller Fehlermeldung
                     
             
-    def transfer (pFile, pTransfer):
+    def transfer (pFile, pTransfer): # Kopieren einer Datei an einen anderen Ort, Dateipfad und Kopierpfad als Parameter
         
         try:
-            shutil.copy(pFile, pTransfer)
+            shutil.copy(pFile, pTransfer) # Kopiere Datei
             #shutil.make_archive(pTransfer,'zip', pTransfer)
         except Exception as e:
-            logging.error(e)
+            logging.error(e) #Logging potentieller Fehlermeldung
             
-    def compress(pTransfer):
+    def compress(pTransfer): # Komprimierung einer Datei, eines Ordners als Zip. Pfad als Parameter
         
         try:
-            shutil.make_archive(pTransfer,'zip', pTransfer)
+            shutil.make_archive(pTransfer,'zip', pTransfer) # Komprimierung, Speicherung des .zip an dem gleichen Ort
         except Exception as e:
-            logging.error(e)
+            logging.error(e) #Logging potentieller Fehlermeldung
