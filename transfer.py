3861c14f9d0e6d736efa716c48c000487fc73e4b
fkallmer <94541193+fkallmer@users.noreply.github.com>
2022-10-03 20:07:58 +0200

    Update transfer.py

--- transfer.py
+++ transfer.py
@@ -1,46 +1,41 @@
-import smtplib
-import mimetypes
-from email.message import EmailMessage
-from datetime import datetime
-from picamera import PiCamera
-from time import sleep
-import os
-import logging
-from folder import *
+import smtplib # SMTP-Bibliothek zum Versenden der Mail
+import mimetypes # Mimtype-Bibliothek, Zuordnung von Dateien zu MIME types (Dateitypen, -endungen)
+from email.message import EmailMessage # E-Mail-Bibliothek, zum Erstellen von versandfähigen E-Mails
+import os # OS-Bibliothek
+import logging # Logging-Bibliothek
+from folder import * # Import der Klasse folder
 
 class transfer():     
 
-    def sendMail(pSubject, pFrom, pTo, pContent, pFile, filename, host, username, password):
-   # def __init__():
-       # logging.basicConfig(filename="mai.log.txt", format="%(asctime)s %(message)s")
-        folder.compress(pFile)
-        pZip = pFile + '.zip'
-        print(pFile)
-        msg = EmailMessage()
-        msg['Subject'] = pSubject
-        msg['From'] = pFrom
-        msg['To'] = pTo
+    def sendMail(pSubject, pFrom, pTo, pContent, pFile, filename, host, username, password): # Definition der Methode zum Versenden der E-Mails
+  
+        folder.compress(pFile) # Komprimierung der zu versendenden Datei/Ordner
+        pZip = pFile + '.zip' # Vervollständigung des Dateipfads der .zip
+        msg = EmailMessage() # Erstellung der E-Mail
+        msg['Subject'] = pSubject #Hinzufügen Betreff
+        msg['From'] = pFrom # Hinzufügen Absender
+        msg['To'] = pTo # Hinzufügen Empfänger
         msg.set_content(pContent) # Inhalt der Mail
             
         try:
-            with open(pZip, 'rb') as fp:
+            with open(pZip, 'rb') as fp: # Öffnen der zu versendenden Datei
                 file_data = fp.read()
-                maintype, _, subtype = (mimetypes.guess_type(pZip)[0] or 'application/octet-stream').partition("/")
-                msg.add_attachment(file_data, maintype=maintype, subtype=subtype, filename=filename)
+                maintype, _, subtype = (mimetypes.guess_type(pZip)[0] or 'application/octet-stream').partition("/") # Auslesen des MIME Types (Dateityp)
+                msg.add_attachment(file_data, maintype=maintype, subtype=subtype, filename=filename) # Anhängen der Datei an die E-Mail
                 
         except Exception as e:
-            logging.error(e)
+            logging.error(e) # Logging potentieller Fehlermeldung
                 
         try:
-            s = smtplib.SMTP(host)
-            s.starttls()
-            s.login(username, password)
-            s.send_message(msg)
-            s.quit()
-            folder.delFolder(pFile)
-            os.remove(pZip)
+            s = smtplib.SMTP(host) # SMTP-URL
+            s.starttls() # Start des TLS-Verschlüsselungsprotokolls
+            s.login(username, password) # Anmeldung mit Benutzernamen und Passwort
+            s.send_message(msg) # Versenden der Nachricht
+            s.quit() # Schließen des SMTP-Vorgangs
+            folder.delFolder(pFile) # Löschen des Transferordners
+            os.remove(pZip) # Löschen der .zip-Datei
         except Exception as e:
-            logging.error(e)
+            logging.error(e) # Logging potentieller Fehlermeldung
             
             
             
